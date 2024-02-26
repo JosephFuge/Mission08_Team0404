2be4831c15c0a6816d5209ab143a8160932791a5
Joseph Fuge <josephfuge1@gmail.com>
2024-02-26 13:29:35 -0700

    Add project files.

---
+++ Mission08_Team0404/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission08_Team0404.Models;
+using System.Diagnostics;
+
+namespace Mission08_Team0404.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+}
