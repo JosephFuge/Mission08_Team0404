32a4d6dffa02e165008fb89b4f0d74ebda8b64ff
Cameron Klepacz <cameron.klepacz@gmail.com>
2024-02-26 21:08:39 -0700

    Downloaded NuGet packages, deleted the ErrorViewModel and ErrorView, set up the connection to the database in appsettings and program.cs, created the UserTask model and contex, did migrations and created the database

---
+++ Mission08_Team0404/Models/UserTaskContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team0404.Models
+{
+    public class UserTaskContext : DbContext
+    {
+        public UserTaskContext(DbContextOptions<UserTaskContext>options) : base(options) 
+        { 
+        }
+
+        public DbSet<UserTask> UserTasks { get; set; }
+    }
+}
