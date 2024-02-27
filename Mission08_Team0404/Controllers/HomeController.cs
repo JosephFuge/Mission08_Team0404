69db52c3fbf8d2826c2e3fea02ab1a534a1937ac
Joseph Fuge <josephfuge1@gmail.com>
2024-02-27 13:59:25 -0700

    Basic controller setup, guesswork

--- Mission08_Team0404/Controllers/HomeController.cs
+++ Mission08_Team0404/Controllers/HomeController.cs
@@ -6,15 +6,65 @@ namespace Mission08_Team0404.Controllers
 {
     public class HomeController : Controller
     {
+        private IUserTaskRepository _repo;
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult CreateTask() { 
+            return View("CreateEditTask"); 
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask(UserTask task) {
+            return View("CreateEditTask");
+        }
+
+        [HttpGet]
+        public IActionResult EditTask(int taskId)
         {
-            return View();
+            UserTask userTask = _repo.UserTasks.FirstOrDefault(x => x.UserTaskId == taskId);
+
+            return View("CreateEditTask", userTask);
+        }
+
+        [HttpPost]
+        public IActionResult EditTask(UserTask task) {
+
+            if (ModelState.IsValid)
+            {
+                _repo.AddUserTask(task);
+            }
+
+            var userTaskList = _repo.UserTasks.ToList();
+
+            return View("TaskQuadrants", userTaskList);
+        }
+
+        public IActionResult TaskQuadrants()
+        {
+            var userTaskList = _repo.UserTasks.ToList();
+
+            return View(userTaskList);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTask(int taskId)
+        {
+            UserTask userTask = _repo.UserTasks.FirstOrDefault(x => x.UserTaskId == taskId);
+
+            return View(userTask);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask(UserTask userTask)
+        {
+            _repo.DeleteTask(userTask);
+
+            return RedirectToAction("TaskQuadrants");
         }
     }
 
