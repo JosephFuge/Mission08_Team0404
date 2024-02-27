634ae2ca509b4ae8522baa52dc9067636c9c7e9c
Cameron Klepacz <cameron.klepacz@gmail.com>
2024-02-27 10:23:24 -0700

    Added Error Messages to the required fields

--- Mission08_Team0404/Models/UserTask.cs
+++ Mission08_Team0404/Models/UserTask.cs
@@ -7,10 +7,10 @@ namespace Mission08_Team0404.Models
         [Key]
         [Required]
         public int TaskId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Task field is required")]
         public string TaskName { get; set; }
         public string? DueDate { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Quadrant field is required")]
         public int Quadrant { get; set; }
         public string? Category { get; set; }
         public bool? Completed { get; set; }
